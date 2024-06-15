7d96524a14f388becb5826e4e2709919b2ff6b90
Zach Foerst <48895051+zachfoerst@users.noreply.github.com>
2024-06-15 10:26:39 -0400

    Create Enumerate.py

---
+++ Enumerate.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+print("\033[92m" + "="*50 + "\033[0m")
+print("\033[91m" + "Enum deez".center(50) + "\033[0m")
+print("\033[91m" + "By: Dark Mattr".center(50) + "\033[0m")
+print("\033[92m" + "="*50 + "\033[0m")
+print()
+print()
+
