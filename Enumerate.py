3f0144815d617a3b1d32becfa0497d2cd06a2712
Zach Foerst <48895051+zachfoerst@users.noreply.github.com>
2024-06-15 11:16:40 -0400

    added sublist3r

--- Enumerate.py
+++ Enumerate.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import sublist3r
 
 print("\033[92m" + "="*50 + "\033[0m")
 print("\033[91m" + "Enum deez".center(50) + "\033[0m")
@@ -11,7 +11,19 @@ print("\033[92m" + "="*50 + "\033[0m")
 print()
 print()
 
-domain = Input(“Which Domain woUld you Likr to pwn today?”)
 
 
+
+domain = input("Which domain would you like to look at today?")
+
+subsSaved = domain + "subs.txt"
+def get_subs():
+    subdomains = sublist3r.main (domain, 40, subsSaved, ports= None, silent=False, verbose= False, enable_bruteforce= False, engines=None)
+
+print(subsSaved)
+subs_list = open(subsSaved).readlines()
+
+
+driver = webdriver.Firefox()
+driver.get()
 
